9e21946b4d4c2d1e6c4033753fa56c3e152d9c5f
Ryan <ryan@renegademinds.com>
2020-03-28 21:58:15 -0300

    Add files

---
+++ CaseRandomizer/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace CaseRandomizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRandomizeCase = new System.Windows.Forms.Button();
+            this.txtInput = new System.Windows.Forms.TextBox();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.scContainer = new System.Windows.Forms.SplitContainer();
+            this.lblInstructions = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.scContainer)).BeginInit();
+            this.scContainer.Panel1.SuspendLayout();
+            this.scContainer.Panel2.SuspendLayout();
+            this.scContainer.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRandomizeCase
+            // 
+            this.btnRandomizeCase.Location = new System.Drawing.Point(12, 12);
+            this.btnRandomizeCase.Name = "btnRandomizeCase";
+            this.btnRandomizeCase.Size = new System.Drawing.Size(368, 49);
+            this.btnRandomizeCase.TabIndex = 0;
+            this.btnRandomizeCase.Text = "Randomize Case";
+            this.btnRandomizeCase.UseVisualStyleBackColor = true;
+            this.btnRandomizeCase.Click += new System.EventHandler(this.btnRandomizeCase_Click);
+            // 
+            // txtInput
+            // 
+            this.txtInput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInput.Location = new System.Drawing.Point(0, 0);
+            this.txtInput.MaxLength = 327670000;
+            this.txtInput.Multiline = true;
+            this.txtInput.Name = "txtInput";
+            this.txtInput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtInput.Size = new System.Drawing.Size(368, 371);
+            this.txtInput.TabIndex = 1;
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(0, 0);
+            this.txtOutput.MaxLength = 327670000;
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtOutput.Size = new System.Drawing.Size(367, 371);
+            this.txtOutput.TabIndex = 2;
+            // 
+            // scContainer
+            // 
+            this.scContainer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.scContainer.Location = new System.Drawing.Point(12, 67);
+            this.scContainer.Name = "scContainer";
+            // 
+            // scContainer.Panel1
+            // 
+            this.scContainer.Panel1.Controls.Add(this.txtInput);
+            // 
+            // scContainer.Panel2
+            // 
+            this.scContainer.Panel2.Controls.Add(this.txtOutput);
+            this.scContainer.Size = new System.Drawing.Size(739, 371);
+            this.scContainer.SplitterDistance = 368;
+            this.scContainer.TabIndex = 3;
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblInstructions.Location = new System.Drawing.Point(386, 12);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(365, 49);
+            this.lblInstructions.TabIndex = 4;
+            this.lblInstructions.Text = "Enter text into the left text box. Click the button. The result will be in the ri" +
+    "ght text box.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 450);
+            this.Controls.Add(this.lblInstructions);
+            this.Controls.Add(this.scContainer);
+            this.Controls.Add(this.btnRandomizeCase);
+            this.Name = "Form1";
+            this.Text = "Case Randomizer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.scContainer.Panel1.ResumeLayout(false);
+            this.scContainer.Panel1.PerformLayout();
+            this.scContainer.Panel2.ResumeLayout(false);
+            this.scContainer.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.scContainer)).EndInit();
+            this.scContainer.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRandomizeCase;
+        private System.Windows.Forms.TextBox txtInput;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.SplitContainer scContainer;
+        private System.Windows.Forms.Label lblInstructions;
+    }
+}
+
---
+++ CaseRandomizer/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CaseRandomizer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        Random r = new Random();
+
+        private void btnRandomizeCase_Click(object sender, EventArgs e)
+        {
+            char[] array = txtInput.Text.ToCharArray();
+            StringBuilder sb = new StringBuilder();
+            foreach (char c in array)
+            {
+                if (r.Next(0, 2) == 0)
+                {
+                    sb.Append(c.ToString().ToUpper());
+                }
+                else {
+                    sb.Append(c.ToString().ToLower());
+                }
+
+            }
+            txtOutput.Text = sb.ToString();
+        }
+    }
+}
